2870f57b1df42450d68e3ff1452c3ac4dde60dbe
Hannah Hepburn <hannahm.hepburn@gmail.com>
2023-02-28 11:05:28 -0500

    Buttons formatting fixed

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+	selector: 'app-button',
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
+	button1: Boolean = true;
+	button2: Boolean = false;
+	button3: Boolean = false;
 
+	setButtonOne() {
+		this.button1 = true;
+		this.button2 = false;
+		this.button3 = false;
+	}
+
+	setButtonTwo() {
+		this.button1 = false;
+		this.button2 = true;
+		this.button3 = false;
+	}
+
+	setButtonThree() {
+		this.button1 = false;
+		this.button2 = false;
+		this.button3 = true;
+	}
 }
