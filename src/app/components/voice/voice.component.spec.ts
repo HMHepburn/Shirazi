73e80eae096918d42d2ef1005fd00f2e3a6cc0ec
Hannah Hepburn <hannahm.hepburn@gmail.com>
2023-03-02 11:57:30 -0500

    changes made after discussion with Mansib

---
+++ src/app/components/voice/voice.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VoiceComponent } from './voice.component';
+
+describe('VoiceComponent', () => {
+  let component: VoiceComponent;
+  let fixture: ComponentFixture<VoiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VoiceComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VoiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/voice/voice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voice',
+  templateUrl: './voice.component.html',
+  styleUrls: ['./voice.component.css']
+})
+export class VoiceComponent {
+
+}
