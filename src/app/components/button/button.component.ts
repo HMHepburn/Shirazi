3b5e0f76529a07d78916fdbaedf18c04ce66793d
ndough <ndough@github.com>
2023-03-06 10:31:24 -0500

    testing

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class ButtonComponent {
 
-}
+  // onButtonGroupClick(e: any){
+  //   let clickedElement = e.target || e.srcElement;
+
+  //   if( clickedElement.nodeName === "BUTTON" ) {
+
+  //     let isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector(".active");
+  //     // if a Button already has Class: .active
+  //     if( isCertainButtonAlreadyActive ) {
+  //       isCertainButtonAlreadyActive.classList.remove("active");
+  //     }
+
+  //     clickedElement.className += " active";
+  //   }
+
+    clickedButton(e: any){console.log(e)
+      let temp= e.srcElement.parentNode;
+      for(let i = 0; i < temp.childNodes.length; i++){
+        if(temp.childNodes[i].nodeName === 'BUTTON'){
+        if(temp.childNodes[i].classList.contains('active')){
+          temp.childNodes[i].classList.remove('active');
+        }  }
+      }
+      e.srcElement.classList.add('active')
+      
+    }
+  }
