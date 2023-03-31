f1933d0b0e29e6a133bddecd4fffa80f39caaf4b
Hannah Hepburn <hannahm.hepburn@gmail.com>
2023-03-31 11:04:01 -0400

    Spinner completed!

--- src/app/components/spinner/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner/spinner.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-spinner',
-  templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.css']
+	selector: 'app-spinner',
+	templateUrl: './spinner.component.html',
+	styleUrls: ['./spinner.component.css']
 })
 export class SpinnerComponent {
+	savingGood: boolean = false;
+	savingBad: boolean = false;
 
+	goodButtonPress() {
+		this.savingGood = true;
+	}
+
+	badButtonPress() {
+		this.savingBad = true;
+	}
 }
